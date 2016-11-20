7ba5ba8112816625f717a36327926d16c17a075f
Tomer Weller <tomer.weller@gmail.com>
2016-11-19 19:23:23 -0800

    fixup rendering refreesh

--- src/cljs/lambduhduh/views.cljs
+++ src/cljs/lambduhduh/views.cljs
@@ -1,13 +1,14 @@
 (ns lambduhduh.views
-  (:require
-    [re-frame.core :refer [subscribe dispatch]]
-    [reagent.core :as r]
-    [lambduhduh.util :refer [log]]))
+                              (:require
+                                [re-frame.core :refer [subscribe dispatch]]
+                                [reagent.core :as r]
+                                [lambduhduh.util :refer [log]]))
 
 (def ace-editor (r/adapt-react-class (aget js/deps "react-ace" "default")))
 
 (defn brick [brick-id]
   (let [brick-code (subscribe [:brick-code brick-id])]
+    (log "rendering brick" brick-id)
     [:div#editor-container {:key brick-id}
      [ace-editor
       {:value @brick-code
@@ -18,12 +19,11 @@
        :on-change #(dispatch [:brick-change-code brick-id %])}]]))
 
 (defn root []
-  (fn []
-    (let [bricks-keys (subscribe [:bricks-keys])]
-      (log "rendering root")
-      (log "bricks-keys" @bricks-keys)
-      [:div [:h1 "Lambduh"]
-       (doall (map (fn [brick-id] (brick brick-id)) @bricks-keys))
-       [:button
-        {:on-click #(dispatch [:brick-add])}
-        "Add code"]])))
+  (let [bricks-keys (subscribe [:bricks-keys])]
+    (log "rendering root")
+    [:div [:h1 "Lambduh"]
+     (doall (map (fn [brick-id] ^{:key brick-id} [brick brick-id]) @bricks-keys))
+     [:button
+      {:on-click #(dispatch [:brick-add])}
+      "Add code"]]))
+
